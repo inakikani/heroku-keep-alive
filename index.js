cb23d625627445be1a8f0e263ebc028032ed4724
inakikani <inakikaninaki@gmail.com>
2021-01-22 21:23:44 +0100

    handle error : > skip after too many failures

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ if(process.env.NODE_ENV === 'development') { require('dotenv').config() }
 const https = require('https')
 const http = require('http')
 
+// Create server
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -13,11 +14,17 @@ const server = http.createServer((req, res) => {
 const pingIntervalMinutes = Number(process.env.PING_INTERVAL_MIN || 25)
 const pingIntervalMilliseconds = 1000 * 60 * pingIntervalMinutes
 const hostsList = process.env.PING_LIST.split(';')
+const commas = new RegExp(',', 'g')
 
+const errorThreshold = 1
+let errors = new Map()
+
+// Start server
 server.listen(process.env.PORT, () => {
-    console.log(`server is listening on port: ${process.env.PORT}\nPings every ${pingIntervalMinutes} minutes\nHosts :\n${hostsList.toString().replace(',','\n')}`)
+    console.log(`server is listening on port: ${process.env.PORT}\nPings every ${pingIntervalMinutes} minutes\nHosts :\n${hostsList.toString().replace(commas, '\n')}`)
 })
 
+// periodically ping
 setInterval(() => {
     for(host of hostsList){
         doPing(host)
@@ -25,32 +32,55 @@ setInterval(() => {
 }, pingIntervalMilliseconds)
 
 function doPing(url){
-    console.time(url)
-    https
-        .get(url)
-        .on('response', res => {
-            const { statusCode } = res;
+    try {
+        
+        if(errors.get(url) >= errorThreshold) { 
+            console.log(`........................\n${url} : failed too many times`)
+            return
+        }
 
-            let error;
+        console.time(url)
+        https
+            .get(url)
+            .on('response', res => {
+                const { statusCode } = res;
 
-            if (statusCode !== 200) {
-                error = new Error(`Request Failed.\nStatus Code: ${statusCode}`);
-            }
-            if (error) {
-                console.error(error.message);
-                // Consume response data to free up memory
-                res.resume();
-                return;
-            }
+                let error;
 
-            res.setEncoding('utf8');
-            let data = '';
-            res.on('data', (chunk) => { data += chunk; });
-            res.on('end', () => {
-                console.timeEnd(url)
-            });
-        })
-        .on('error', (e) => {
-            console.error(`Got error: ${e.message}`);
-        })
+                if (statusCode !== 200) {
+                    error = new Error(`Request Failed.\nStatus Code: ${statusCode}`);
+                }
+                if (error) {
+                    handleError(error, url)
+                    // Consume response data to free up memory
+                    res.resume();
+                    return;
+                }
+
+                res.setEncoding('utf8');
+                let data = '';
+                res.on('data', (chunk) => { data += chunk; });
+                res.on('end', () => {
+                    console.log('........................')
+                    console.timeEnd(url)
+                });
+            })
+            .on('error', (e) => {
+                handleError(e, url)
+            })   
+    } catch (error) {
+        handleError(error, url)
+    }
+}
+
+function handleError(error, url){
+    if(errors.has(url)){
+        errors.set(url, errors.get(url) + 1)
+    } else {
+        errors.set(url, 0)
+    }
+
+    console.log('........................\nskip on error ...')
+    console.timeEnd(url)
+    console.log(error)
 }
